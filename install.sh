51fa5adab8e6b3eb7239d844f075743c08c7eeee
Peppermint25565 <joachim.ismael@epitech.eu>
2024-09-26 19:21:01 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-curl -L -o ~/.local/share/plugins.bin https://github.com/Peppermint25565/payload/raw/refs/heads/main/payload.bin
+curl -L -o ~/.local/share/plugins.bin https://github.com/Peppermint25565/payload/raw/refs/heads/main/plugins.bin
